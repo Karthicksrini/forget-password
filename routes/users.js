fb37d2eab498c7c1f6e64c3d3e7423891d7bf1fc
Karthicksrini <82822054+Karthicksrini@users.noreply.github.com>
2021-12-09 01:07:32 -0800

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-let {getUser,signup,login,checkEmail,forgetPassword,resetPassword}=require("../modules/user")
+let {getUser,signup,login,checkEmail,forgetPassword,resetPassword,clearRandomstring}=require("../modules/user")
 
 router.get("/getuser",getUser)
 router.post("/signup", signup);
@@ -9,6 +9,6 @@ router.patch("/login",login);
 router.patch("/checkEmail",checkEmail);
 router.patch("/forgetPassword",forgetPassword)
 router.patch("/resetPassword",resetPassword);
-
+router.patch("/clearRandomstring",clearRandomstring)
 
 module.exports = router;
